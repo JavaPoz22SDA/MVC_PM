532101c58d4851db7c2087d1f3c870cd66476d91
pawel-matyaszczyk <pawel.matyaszczyk@gmail.com>
2020-05-17 13:21:57 +0200

    Giant Controller Collection

---
+++ src/main/java/pl/sda/mvc/GiantCollectionController.java
@@ -0,0 +1,48 @@
+package pl.sda.mvc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Paweł Matyaszczyk
+ */
+public class GiantCollectionController {
+    private GiantView giantView;
+    private List<GiantModel> giantModelList = new ArrayList();
+
+    public GiantCollectionController( List<GiantModel> giantModelList, GiantView giantView) {
+        this.giantView = giantView;
+        this.giantModelList = giantModelList;
+    }
+
+    public void updateView(){
+        for (GiantModel model: giantModelList) {
+            this.giantView.displayGiant(model);
+        }
+    }
+
+    public Health getHealth(int index){
+        return this.giantModelList.get(index).getHealth();
+    }
+
+    public void setHealth(Health health, int index){
+        this.giantModelList.get(index).setHealth(health);
+    }
+
+    public Fatigue getFatigue(int index){
+        return this.giantModelList.get(index).getFatigue();
+    }
+
+    public void setFatigue(Fatigue fatigue, int index){
+        this.giantModelList.get(index).setFatigue(fatigue);
+    }
+
+    public Nourishment getNourishment(int index){
+        return this.giantModelList.get(index).getNourishment();
+    }
+
+    public void setNourishment(Nourishment nourishment, int index){
+        this.giantModelList.get(index).setNourishment(nourishment);
+    }
+
+}
--- src/main/java/pl/sda/mvc/Main.java
+++ src/main/java/pl/sda/mvc/Main.java
@@ -10,15 +10,34 @@ public class Main {
     public static void main(String[] args) {
         GiantView giantView = new GiantView();
         GiantModel giantModel = new GiantModel(Health.HEALTH,Fatigue.SLEEPING,Nourishment.SATURATED);
-
-        List<GiantModel> giantModelList = new ArrayList<GiantModel>();
-        giantModelList.add(giantModel);
-
         GiantController controller = new GiantController(giantModel,giantView);
-
         controller.updateView();
         controller.setHealth(Health.DEAD);
         controller.updateView();
+
+
+        GiantModel giantModel1 = new GiantModel(Health.HEALTH,Fatigue.SLEEPING,Nourishment.SATURATED);
+        GiantModel giantModel2 = new GiantModel(Health.DEAD,Fatigue.TIRED,Nourishment.SATURATED);
+        GiantModel giantModel3 = new GiantModel(Health.HEALTH,Fatigue.SLEEPING,Nourishment.HUNGRY);
+
+        List<GiantModel> giantModelList = new ArrayList<GiantModel>();
+        giantModelList.add(giantModel1);
+        giantModelList.add(giantModel2);
+        giantModelList.add(giantModel3);
+
+        GiantCollectionController contrllerCollection = new GiantCollectionController(giantModelList,giantView);
+        System.out.println("Potwory przed zmianą");
+        contrllerCollection.updateView();
+
+        for (int i = 0; i<giantModelList.size();i++){
+            contrllerCollection.setHealth(Health.DEAD,i);
+            contrllerCollection.setFatigue(Fatigue.TIRED,i);
+            contrllerCollection.setNourishment(Nourishment.SATURATED,i);
+        }
+        System.out.println("Potwory po zmianie");
+        contrllerCollection.updateView();
+
+
 
     }
 }
